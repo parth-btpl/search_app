f9abf8ad1a5d1d44ddf39aded32ce9bc6ddb0f56
parth-btpl <parth.patel@botreetechnologies.com>
2020-06-16 10:48:07 +0530

    setup database, create user model and migration.

---
+++ db/migrate/20200616051326_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.integer :age
+      t.string :profession
+      t.text :about
+
+      t.timestamps
+    end
+  end
+end
