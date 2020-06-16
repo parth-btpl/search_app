90bdb29e4b8b1c7b48b061c0485cc0da5fc86a10
parth-btpl <parth.patel@botreetechnologies.com>
2020-06-16 12:06:09 +0530

    word start and indexing params.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-  searchkick
+  searchkick word_start: [:first_name, :last_name, :email]
+
+  # CONTROL WHAT ELASTIC SEARCH IS INDEXING
+  def search_data
+    {
+      first_name: first_name, 
+      last_name: last_name,
+      email: email
+    }
+  end
 end
